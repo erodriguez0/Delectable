f651f036e7109e9046f5a1be93baedfe7e590b72
Esteban Rodriguez <erodriguez0705@users.noreply.github.com>
2019-11-09 01:05:53 -0800

    Initial laravel project setup

---
+++ resources/assets/js/app.js
@@ -0,0 +1,6 @@
+require('./bootstrap');
+// require('jquery');
+// require('popper.js');
+// require('./../../../node_modules/mdbootstrap/js/bootstrap');
+// require('./../../../node_modules/mdbootstrap/js/mdb');
+// require('./_custom');
