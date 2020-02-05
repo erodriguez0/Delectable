4c92a53011afba0aac0b57904dc46ec3818fc577
Esteban Rodriguez <9098670+erodriguez0@users.noreply.github.com>
2020-02-05 02:53:46 -0800

    Updated CSS

--- inc/header.php
+++ inc/header.php
@@ -2,6 +2,10 @@
 <html lang="en-us">
 <head>
 	<title><?php echo $title; ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+	<meta http-equiv="Pragma" content="no-cache" />
+	<meta http-equiv="Expires" content="0" />
 	<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="./css/custom.css">
