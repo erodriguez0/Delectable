b08e08e6d6762c830f9d46c95249e2a16cf94543
Esteban Rodriguez <erodriguez0705@users.noreply.github.com>
2020-02-14 16:26:45 -0800

    admin login implemented, redirect to dashboard upon login

--- public_html/admin/index.php
+++ public_html/admin/index.php
@@ -15,9 +15,14 @@ require_once(INCLUDE_PATH . 'header.php');
 			<div class="row">
 				<div class="col-12">
 					<div class="admin-form-wrap mx-auto">
-						<input class="form-control mb-3" type="text" placeholder="Username" value="<?php echo $_SESSION['login']['uname']; ?>" required>
-						<input class="form-control mb-3" type="password" placeholder="Password" required>
-						<input class="btn btn-primary btn-block" type="submit" value="Login">
+						<?php if(isset($_SESSION['error'])): ?>
+							<div class="alert alert-danger">
+								<?php echo $_SESSION['error']; ?>
+							</div>
+						<?php endif; ?>
+						<input class="form-control mb-3" type="text" placeholder="Username" value="<?php echo $_SESSION['login']['uname']; ?>" name="admin-username" required>
+						<input class="form-control mb-3" type="password" placeholder="Password" name="admin-password" required>
+						<input class="btn btn-primary btn-block" type="submit" value="Login" name="admin-login">
 					</div>
 				</div>
 			</div>
@@ -26,6 +31,7 @@ require_once(INCLUDE_PATH . 'header.php');
 </div>
 
 <?php
+unset($_SESSION['error']);
 require_once(INCLUDE_PATH . 'footer.php');
 endif;
 ?>
