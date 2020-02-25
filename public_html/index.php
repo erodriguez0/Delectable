b3901a10b0ac99f2047603d3a88ded4a5a4b9dd7
Esteban Rodriguez <9098670+erodriguez0@users.noreply.github.com>
2020-02-25 02:03:40 -0800

    Minor landing page styling

--- public_html/index.php
+++ public_html/index.php
@@ -14,7 +14,7 @@ require_once(INCLUDE_PATH . 'header.php');
 <!-- Navigation -->
 <nav id="home-nav" class="navbar navbar-expand-lg navbar-dark shadow fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#">Delectable</a>
+        <a class="navbar-brand text-uppercase" href="#">Delectable</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
@@ -35,17 +35,23 @@ require_once(INCLUDE_PATH . 'header.php');
 </nav>
 <!-- Full Page Image Header with Vertically Centered Content -->
 <header class="masthead">
+<div class="overlay">
     <div class="container h-100">
         <div class="row h-100 align-items-center">
             <div class="col-12 text-center">
-                <h1 class="font-weight-light">Delectable</h1>
-                <p class="lead">A great starter layout for a landing page</p>
+                <h1 class="font-weight-light text-white text-uppercase">
+                    Delectable
+                </h1>
+                <p class="lead text-white">
+                    Skip the line. Table reservations at your fingertips.
+                </p>
                 <div class="masthead-input mx-auto w-50">
-                    <input class="form-control" type="text" name="restaurant-search" placeholder="Search for restaurants">
+                    <input class="form-control" type="text" name="restaurant-search" placeholder="Search restaurants">
                 </div>
             </div>
         </div>
     </div>
+</div>
 </header>
 <!-- Page Content -->
 <section class="py-5">
