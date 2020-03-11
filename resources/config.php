395fcb5f1ddaf53c5187c8cdaa538d2b9dfdaedb
Esteban Rodriguez <erodriguez0705@gmail.com>
2020-03-11 02:32:56 -0700

    Bug fixes for layout.js. Moved to assets/js folder. JS scripts loaded using PHP.

--- resources/config.php
+++ resources/config.php
@@ -22,4 +22,16 @@ define(RESOURCE_PATH, $_SERVER['DOCUMENT_ROOT'] . '/delectable/resources/');
 define(INCLUDE_PATH, $_SERVER['DOCUMENT_ROOT'] . '/delectable/resources/');
 define(HOME, $_SERVER['DOCUMENT_ROOT'] . '/delectable/public_html/');
 define(SCRIPTS_PATH, $_SERVER['DOCUMENT_ROOT'] . '/delectable/resources/scripts/');
+define(JS_PATH, '/delectable/public_html/assets/js/');
+
+$scripts = array();
+$scripts[] = '<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>';
+$scripts[] = '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>';
+$scripts[] = '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>';
+$scripts[] = '<script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>';
+$scripts[] = '<script> feather.replace(); </script>';
+$scripts[] = '<script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.7.11/fabric.min.js"></script>';
+$scripts[] = '<script src="//cdn.bootcss.com/noUiSlider/8.5.1/nouislider.js"></script>';
+$scripts[] = '<script type="text/javascript" src="' . JS_PATH . '/layout.js?v=' . uniqid() . '"></script>';
+$scripts[] = '<script src="' . JS_PATH . 'custom.js?v=' . uniqid() . '" type="text/javascript"></script>';
 ?>
