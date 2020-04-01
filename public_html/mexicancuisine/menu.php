3c1f9500e48f1a3b368f8b7dffcb2922a153ea72
miguz1329 <35708461+miguz1329@users.noreply.github.com>
2020-03-31 23:14:00 -0700

    added restaurant

---
+++ public_html/mexicancuisine/menu.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>The Mexican Cuisine</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+
+    <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="fonts/flaticons/font/flaticon.css">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+<section class="section bg-light element-animate">
+
+      <div class="clearfix mb-5 pb-5">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12 text-center heading-wrap">
+              <h2></h2>
+              <span class="back-text-dark">Menu</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        
+        <div class="row no-gutters">
+          <div class="col-md-6">
+            <div class="sched d-block d-lg-flex">
+              <div class="bg-image order-2" style="background-image: url('img/dishes_4.jpg');"></div>
+              <div class="text order-1">
+                <h3>Grilled Caesar salad, shaved reggiano</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo delectus...</p>
+                <p class="text-primary h3">$12.00</p>
+              </div>
+              
+            </div>
+
+            <div class="sched d-block d-lg-flex">
+              <div class="bg-image" style="background-image: url('img/dishes_1.jpg');"></div>
+              <div class="text">
+                <h3>Spicy Calamari and beans</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo delectus...</p>
+                <p class="text-primary h3">$12.00</p>
+                
+              </div>
+              
+            </div>
+
+          </div>
+
+          <div class="col-md-6">
+            <div class="sched d-block d-lg-flex">
+              <div class="bg-image order-2" style="background-image: url('img/dishes_2.jpg');"></div>
+              <div class="text order-1">
+                <h3>Bacon wrapped wild gulf prawns</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo delectus...</p>
+                <p class="text-primary h3">$18.00</p>
+                
+              </div>
+              
+            </div>
+
+            <div class="sched d-block d-lg-flex">
+              <div class="bg-image" style="background-image: url('img/dishes_3.jpg');"></div>
+              <div class="text">
+                <h3>Seared ahi tuna fillet*, honey-ginger sauce</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo delectus...</p>
+                <p class="text-primary h3">$16.00</p>
+                
+              </div>
+              
+            </div>
+
+          </div>
+        </div>
+        
+
+      </div>
+    </section> <!-- .section -->
+<!-- loader -->
+    <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#cf1d16"/></svg></div>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/magnific-popup-options.js"></script>
+    
+
+    <script src="js/main.js"></script>
+  </body>
+</html>
