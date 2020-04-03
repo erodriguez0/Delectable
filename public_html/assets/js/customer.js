c72ef4299d5bd1b2e88bcda93dd5528c3d6c53ca
AlfredoZavala <azavala5@csub.edu>
2020-04-02 17:50:37 -0700

    Added state selectiion

--- public_html/assets/js/customer.js
+++ public_html/assets/js/customer.js
@@ -3,75 +3,77 @@ $(document).ready(function() {
 		$("#create-account-modal").modal('show');
 	}
 
-	var usStates = [
-		{ name: 'ALABAMA', abbreviation: 'AL'},
-		{ name: 'ALASKA', abbreviation: 'AK'},
-		{ name: 'AMERICAN SAMOA', abbreviation: 'AS'},
-		{ name: 'ARIZONA', abbreviation: 'AZ'},
-		{ name: 'ARKANSAS', abbreviation: 'AR'},
-		{ name: 'CALIFORNIA', abbreviation: 'CA'},
-		{ name: 'COLORADO', abbreviation: 'CO'},
-		{ name: 'CONNECTICUT', abbreviation: 'CT'},
-		{ name: 'DELAWARE', abbreviation: 'DE'},
-		{ name: 'DISTRICT OF COLUMBIA', abbreviation: 'DC'},
-		{ name: 'FEDERATED STATES OF MICRONESIA', abbreviation: 'FM'},
-		{ name: 'FLORIDA', abbreviation: 'FL'},
-		{ name: 'GEORGIA', abbreviation: 'GA'},
-		{ name: 'GUAM', abbreviation: 'GU'},
-		{ name: 'HAWAII', abbreviation: 'HI'},
-		{ name: 'IDAHO', abbreviation: 'ID'},
-		{ name: 'ILLINOIS', abbreviation: 'IL'},
-		{ name: 'INDIANA', abbreviation: 'IN'},
-		{ name: 'IOWA', abbreviation: 'IA'},
-		{ name: 'KANSAS', abbreviation: 'KS'},
-		{ name: 'KENTUCKY', abbreviation: 'KY'},
-		{ name: 'LOUISIANA', abbreviation: 'LA'},
-		{ name: 'MAINE', abbreviation: 'ME'},
-		{ name: 'MARSHALL ISLANDS', abbreviation: 'MH'},
-		{ name: 'MARYLAND', abbreviation: 'MD'},
-		{ name: 'MASSACHUSETTS', abbreviation: 'MA'},
-		{ name: 'MICHIGAN', abbreviation: 'MI'},
-		{ name: 'MINNESOTA', abbreviation: 'MN'},
-		{ name: 'MISSISSIPPI', abbreviation: 'MS'},
-		{ name: 'MISSOURI', abbreviation: 'MO'},
-		{ name: 'MONTANA', abbreviation: 'MT'},
-		{ name: 'NEBRASKA', abbreviation: 'NE'},
-		{ name: 'NEVADA', abbreviation: 'NV'},
-		{ name: 'NEW HAMPSHIRE', abbreviation: 'NH'},
-		{ name: 'NEW JERSEY', abbreviation: 'NJ'},
-		{ name: 'NEW MEXICO', abbreviation: 'NM'},
-		{ name: 'NEW YORK', abbreviation: 'NY'},
-		{ name: 'NORTH CAROLINA', abbreviation: 'NC'},
-		{ name: 'NORTH DAKOTA', abbreviation: 'ND'},
-		{ name: 'NORTHERN MARIANA ISLANDS', abbreviation: 'MP'},
-		{ name: 'OHIO', abbreviation: 'OH'},
-		{ name: 'OKLAHOMA', abbreviation: 'OK'},
-		{ name: 'OREGON', abbreviation: 'OR'},
-		{ name: 'PALAU', abbreviation: 'PW'},
-		{ name: 'PENNSYLVANIA', abbreviation: 'PA'},
-		{ name: 'PUERTO RICO', abbreviation: 'PR'},
-		{ name: 'RHODE ISLAND', abbreviation: 'RI'},
-		{ name: 'SOUTH CAROLINA', abbreviation: 'SC'},
-		{ name: 'SOUTH DAKOTA', abbreviation: 'SD'},
-		{ name: 'TENNESSEE', abbreviation: 'TN'},
-		{ name: 'TEXAS', abbreviation: 'TX'},
-		{ name: 'UTAH', abbreviation: 'UT'},
-		{ name: 'VERMONT', abbreviation: 'VT'},
-		{ name: 'VIRGIN ISLANDS', abbreviation: 'VI'},
-		{ name: 'VIRGINIA', abbreviation: 'VA'},
-		{ name: 'WASHINGTON', abbreviation: 'WA'},
-		{ name: 'WEST VIRGINIA', abbreviation: 'WV'},
-		{ name: 'WISCONSIN', abbreviation: 'WI'},
-		{ name: 'WYOMING', abbreviation: 'WY' }
-	];
 	
-	
-	for(var i = 0;i<usStates.length;i++){
-		var option = document.createElement("option");
-		option.text = usStates[i].name+' ['+usStates[i].abbreviation+']';
-		option.value = i;
-		var select = document.getElementById("state");
-		select.appendChild(option);
-	}
 	
 });
+
+var usStates = [
+	{ name: 'ALABAMA'},
+	{ name: 'ALASKA'},
+	{ name: 'AMERICAN SAMOA'},
+	{ name: 'ARIZONA'},
+	{ name: 'ARKANSAS'},
+	{ name: 'CALIFORNIA'},
+	{ name: 'COLORADO'},
+	{ name: 'CONNECTICUT'},
+	{ name: 'DELAWARE'},
+	{ name: 'DISTRICT OF COLUMBIA'},
+	{ name: 'FEDERATED STATES OF MICRONESIA'},
+	{ name: 'FLORIDA'},
+	{ name: 'GEORGIA'},
+	{ name: 'GUAM'},
+	{ name: 'HAWAII'},
+	{ name: 'IDAHO'},
+	{ name: 'ILLINOIS'},
+	{ name: 'INDIANA'},
+	{ name: 'IOWA'},
+	{ name: 'KANSAS'},
+	{ name: 'KENTUCKY'},
+	{ name: 'LOUISIANA'},
+	{ name: 'MAINE'},
+	{ name: 'MARSHALL ISLANDS'},
+	{ name: 'MARYLAND'},
+	{ name: 'MASSACHUSETTS'},
+	{ name: 'MICHIGAN'},
+	{ name: 'MINNESOTA'},
+	{ name: 'MISSISSIPPI'},
+	{ name: 'MISSOURI'},
+	{ name: 'MONTANA'},
+	{ name: 'NEBRASKA'},
+	{ name: 'NEVADA'},
+	{ name: 'NEW HAMPSHIRE'},
+	{ name: 'NEW JERSEY'},
+	{ name: 'NEW MEXICO'},
+	{ name: 'NEW YORK'},
+	{ name: 'NORTH CAROLINA'},
+	{ name: 'NORTH DAKOTA'},
+	{ name: 'NORTHERN MARIANA ISLANDS'},
+	{ name: 'OHIO'},
+	{ name: 'OKLAHOMA'},
+	{ name: 'OREGON'},
+	{ name: 'PALAU'},
+	{ name: 'PENNSYLVANIA'},
+	{ name: 'PUERTO RICO'},
+	{ name: 'RHODE ISLAND'},
+	{ name: 'SOUTH CAROLINA'},
+	{ name: 'SOUTH DAKOTA'},
+	{ name: 'TENNESSEE'},
+	{ name: 'TEXAS'},
+	{ name: 'UTAH'},
+	{ name: 'VERMONT'},
+	{ name: 'VIRGIN ISLANDS'},
+	{ name: 'VIRGINIA'},
+	{ name: 'WASHINGTON'},
+	{ name: 'WEST VIRGINIA'},
+	{ name: 'WISCONSIN'},
+	{ name: 'WYOMING' }
+];
+
+
+for(var i = 0;i<usStates.length;i++){
+	var option = document.createElement("option");
+	option.text = usStates[i].name;
+	option.value = i;
+	var select = document.getElementById("state");
+	select.appendChild(option);
+}
